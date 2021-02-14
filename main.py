daee889cf3e59afbdf9dc99ac04d51605bb8707a
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-02-14 11:30:25 -0500

    Computer Guess the Number

--- main.py
+++ main.py
@@ -13,4 +13,20 @@ def guess(x):
             print("Too high, guess again!")
     print(f"Congrats! The number was {random_number}")
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    value = ''
+    while value != 'c':
+        if low == high:
+            guess = low
+        else:
+            guess = random.randint(low,high)
+        value = input(f'I guessed {guess}! Is it too high(H), too low(L), or correct(C)?: ').lower()
+        if value == 'h':
+            high = guess - 1
+        elif value == 'l':
+            low = guess + 1
+    print(f"Hooray! I guessed the number {guess} correctly!")
+
+computer_guess(1000)
