39dad9040997dab7475bd0d095dafc1faa081eb5
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-02-14 11:52:09 -0500

    Rock, Paper, Scissors

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-### Guess the Number ###
+### Rock Paper Scissors ###
 import random
 
-def guess(x):
-    random_number = random.randint(1,x)
-    guess = 0;
-    while guess != random_number:
-        guess = int(input(f'Guess the number I\'m thinking of from 1 and {x}: '))
-        print(guess)
-        if guess < random_number:
-            print("Too low, guess again!")
-        elif guess > random_number:
-            print("Too high, guess again!")
-    print(f"Congrats! The number was {random_number}")
 
-def computer_guess(x):
-    low = 1
-    high = x
-    value = ''
-    while value != 'c':
-        if low == high:
-            guess = low
-        else:
-            guess = random.randint(low,high)
-        value = input(f'I guessed {guess}! Is it too high(H), too low(L), or correct(C)?: ').lower()
-        if value == 'h':
-            high = guess - 1
-        elif value == 'l':
-            low = guess + 1
-    print(f"Hooray! I guessed the number {guess} correctly!")
+def play():
+    user = input('What\'s your choice? "r" for rock, "p" for paper, "s" for scissors? \n')
+    computer = random.choice(["r", "p", "s"])
+    if user == computer:
+        return print('It\'s a tie!')
+    if winner(user, computer):
+        return print('You won!')
+    return print('You lost!')
 
-computer_guess(1000)
+
+# p for player, c for computer
+def winner(p, c):
+    if (p == 'r' and c == 's') or (p == 's' and c == 'p') or (p == 'p' and c == 'r'):
+        return True
+
+
+play()
