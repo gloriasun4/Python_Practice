c082bda630e04dd21dc27cc2de818351a8f16a3b
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-02-19 09:31:14 -0500

    Java to Python (pt. 3) Tuples

--- main.py
+++ main.py
@@ -1,45 +1,17 @@
 ### Java vs. Python ###
 
-# Lists
+# Tuples
 
 # Java
-# List list = new ArrayList();
-# list.add("house");
-# list.add("mouse");
-# list.add("blouse");
-# System.out.println(list);
-# System.out.println(list.size());
-# System.out.println(list.get(1));
-# list.add(1, "grouse")
-# System.out.println(list)
-# list.remove(1);
-# System.out.println(list)
+# Immutable Lists
 
 # Python
-# list = []
-# list.append("house")
-# list.append("mouse")
-# list.append("blouse")
-# print(list)
-# print(len(list))
-# print(list[1])
-# list.insert(1, "grouse")
-# print(list)
-# del(list[1:3])
-# print(list)
+# z = 2,3,4,5
+# x = "house", "moose", "goose"
+# y = x, 5, "Yes"
+# print(y[1:3])
+#Tuple with one element
+# a = 5,
+# print("house" in x)
+# print
 
-# list2 = []
-# list2.append("house")
-# list2.append("mouse")
-# list2.append("blouse")
-# list1.extend(list2)
-# print(list1)
-# list3 = list + list2
-# print(list3)
-# list4 = []
-# list4.append(list)
-# list4.append(list2)
-# print(list4)
-# print(list4[1][2])
-# list4[1] = 4
-# print(list4)
