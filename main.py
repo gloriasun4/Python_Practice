c12491ce0a9f98777f7672412b6de2cbd040fa0f
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-02-19 09:50:38 -0500

    Java to Python (pt. 5) if statements

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 ### Java vs. Python ###
 
-# Checking Equality
+# if statements
 
 # Java
-# Scanner reader = new Scanner();
-# String input = reader.nextLine();
-# String str = "house"
-# Even if you type "house" into the console,
-# will return false because not the same instance
-# System.out.println(str == input);
-# System.out.println(str.equals(input));
-# .equals() is usually modified in the class
+# if(3 > 4) {
+# System.out.println("1");
+# }
+# else if (4 > 3) {
+# System.out.println("2");
+# }
+# else {
+# System.out.println("3");
+# }
 
 # Python
-# x = input()
-# y = "house"
-# checks if exact same values
-# print(x == y)
-# checks if exact same objects
-# print(x is y)
+# if 3 > 4:
+#     print('1')
+# elif 4 > 3:
+#     print('2')
+# else:
+#     print('3')
 
-#Operators
-# <, >, <=, >=, != are the exact same in both!
-
-# Java
-# System.out.println(!(4 <5));
-# !, &&, ||
-
-# Python
-# print(not(4 < 5))
-# not, and, or
