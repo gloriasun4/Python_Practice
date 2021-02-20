b41520c753b722f46659560dc7e68e13af02e9a9
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-02-20 11:29:31 -0500

    Java to Python (pt. 7) functions, method overloading

--- main.py
+++ main.py
@@ -1,56 +1,27 @@
 ### Java vs. Python ###
 
-# while loops
+# functions, method overloading
 
 # Java
-# int x = 0;
-# while (x < 5) {
-#   System.out.print(x + " ");
-#   x++;
+# private static int sum(int a, int b, int c) {
+#   return a + b + c;
+# }
+# private static int sum(int a, int b) {
+#   return a + b;
+# }
+# public static void main(String[] args) {
+#   System.out.println(sum(1,2,3));
 # }
 
 # Python
-# x = 0
-# while x<5:
-#     print(x, end = " ")
-#     x+=1
-
-# for loops
-
-# Java
-# for (int i = 0; i < 5; i++) {
-#   System.out.print(i + " ");
-# }
-
-# Python
-# range goes from 0 to whatever is in range, exclusive
-# can also put a starting value ex. range (2,10)
-# increment range(2, 10, 3)
-# for x in range(5):
-#     print(x, end = " ")
-
-# for-each loops, break and continue statements
-
-# Java
-# int[] array = {1,2,3,4};
-# for(i in array) {
-#   System.out.print(i + " ");
-# }
-
-# Python
-# list = [1,2,3,4]
-# for x in list:
-#     if x == 3:
-# can replace break with continue
-#         break
-#     print(x, end=" ")
-
-# Python ONLY: else off of for loop
-# else body is execute if the loop is iterated fully
-# in this ex., if x == 3, then else will never be executed
-# for x in range(5):
-#     if x == 6:
-#         break
-# else:
-#     print('else entered')
+# can define other variables to same function
+# def sum(a, b, c):
+#     return a + b + c
+# add = sum
+# print(add(1,2,3))
+# print(sum(1,2,3))
+# defaulting parameters instead of method overloading
+# if c is not entered, default value is 0
+# def sum2(a, b, c =0):
+#     return a + b + c
 
