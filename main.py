80f6c7957ae096ac146b94719e83b992e9c02d1e
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-02-25 17:19:59 -0500

    Algorithms (pt. 4) & Unity Beginner Scripting (pt. 1)

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def func_constant(values):
 
 
 func_constant([1, 2, 3, 4, 5, 6, 7, 8])
+
+
 # no matter how large the list, will always return only the first element
 
 # O(n) linear
@@ -24,5 +26,44 @@ def func_linear(values):
         print(x)
 
 
-func_linear([1,2,3,4])
+func_linear([1, 2, 3, 4])
+
+
 # the larger the list, the bigger the BigO, scales linearly with n
+
+# O(n^2) quadratic
+# prints pairs for every element in list
+
+
+def func_quad(values):
+    for x in values:
+        for y in values:
+            print(x, y)
+
+
+func_quad([1, 2, 3])
+""" algo operates n times n assignments, so for a length 3 list, 3^2 is 9
+two loops, one nested inside the other"""
+
+
+# Calculating the Scale of Big O
+# insignificant terms drop out of the notation
+# like taking limits of n to infinity
+
+# Algo with 3n operations is O(n). constants are insignificant because 3(infinity) is the same as infinity
+
+# Example algo with O(1 + n/2 + 10) is just O(n/2)
+
+
+def func_linear_mix(values):
+    print(values[0])
+
+    mid = len(values) // 2
+    for x in values[:mid]:
+        print(x)
+
+    for x in range(10):
+        print("Hello")
+
+
+func_linear_mix([1, 2, 3, 4, 5, 6, 7, 8])
