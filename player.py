cb8c6ee7bf1fe818439211f1b96153dbdabeeac9
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-03-08 11:48:02 -0500

    Tic-Tac-Toe pt. 2

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import math
 import random
 
-#constructer in Python
+
+# constructor in Python
 
 class Player:
     def __init__(self, letter):
@@ -10,16 +11,30 @@ class Player:
     def get_move(self, game):
         pass
 
+
 class RandomComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        # try, except format good for looping until valid value is passed
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again!')
+        return val
