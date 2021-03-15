c814a4783e904eadfd55e3eb31759a0035fddd7d
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-03-14 22:15:55 -0400

    Challenges (pt. 1): #20

--- main.py
+++ main.py
@@ -1,55 +1,25 @@
-### Non-Repeat Elements ###
+### Challenges ###
+# source: https://www.w3resource.com/python-exercises/challenges/1/index.php
 
 """
-Take a string and return character that never repeats
-if multiple uniques then return only the first unique
+20. Write a Python program to check a sequence of numbers is an arithmetic progression or not.
+Input : [5, 7, 9, 11]
+Output : True
+In mathematics, an arithmetic progression or arithmetic sequence is a sequence of numbers such that the difference between the consecutive terms is constant.
+For example, the sequence 5, 7, 9, 11, 13, 15 ... is an arithmetic progression with common difference of 2.
 """
 
 
-# just returning the first unique character
+def arith_progression(lst):
+    if len(lst) < 2:
+        return False
+    diff = lst[1] - lst[0]
 
-# def unique(s):
-#     s = s.replace(" ", "").lower()
-#     chars = {}
-#
-#     for letter in s:
-#         if letter in chars:
-#             chars[letter] += 1
-#         else:
-#             chars[letter] = 1
-#
-#     for letter in s:
-#         if chars[letter] == 1:
-#             return letter
-#
-#     return None
+    for i in range(len(lst) - 1):
+        if lst[i + 1] - lst[i] != diff:
+            return False
+
+    return True
 
 
-# print(unique("I Peel Ape Bananas"))
-
-
-# returning all unique characters
-
-def unique2(s):
-    s = s.replace(" ", "").lower()
-    chars = {}
-
-    for letter in s:
-        if letter in chars:
-            chars[letter] += 1
-        else:
-            chars[letter] = 1
-
-    all_uniques = []
-
-# sorts the dictionary by key value
-# y = sorted(chars.items(), key=lambda x: x[1])
-
-    for letter in s:
-        if chars[letter] == 1:
-            all_uniques.append(letter)
-
-    return all_uniques
-
-
-print(unique2("I Peel Ape Bananas"))
+print(arith_progression([5, 7, 9, 11]))
