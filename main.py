fba75d73bf4a9f64c4facf919f62701270568078
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-03-15 11:11:23 -0400

    Challenges (pt. 2): #31 (pt. 1)

--- main.py
+++ main.py
@@ -2,24 +2,63 @@
 # source: https://www.w3resource.com/python-exercises/challenges/1/index.php
 
 """
-20. Write a Python program to check a sequence of numbers is an arithmetic progression or not.
-Input : [5, 7, 9, 11]
-Output : True
-In mathematics, an arithmetic progression or arithmetic sequence is a sequence of numbers such that the difference between the consecutive terms is constant.
-For example, the sequence 5, 7, 9, 11, 13, 15 ... is an arithmetic progression with common difference of 2.
+31. Write a Python program to add two binary numbers.
+Input : ('11', '1')
+Output : 100
 """
 
 
-def arith_progression(lst):
-    if len(lst) < 2:
-        return False
-    diff = lst[1] - lst[0]
+def add_bin(b1, b2):
+    b1_digit = b1 % 10
+    b2_digit = b2 % 10
+    b1_num = b1[:-1]
+    b2_num = b2[:-1]
+    sum = 0
+    carry = 0
+    while len(b1_num) > 0 & len(b2_num) > 0:
 
-    for i in range(len(lst) - 1):
-        if lst[i + 1] - lst[i] != diff:
-            return False
+        if b1_digit == 1 & b2_digit == 1:
+            carry = 1
 
-    return True
+    return sum
+
+# solution
+
+def add_binary_nums(x, y):
+    max_len = max(len(x), len(y))
+
+    x = x.zfill(max_len)
+    y = y.zfill(max_len)
+
+    result = ''
+    carry = 0
+
+    for i in range(max_len - 1, -1, -1):
+        r = carry
+        r += 1 if x[i] == '1' else 0
+        r += 1 if y[i] == '1' else 0
+        result = ('1' if r % 2 == 1 else '0') + result
+        carry = 0 if r < 2 else 1
+
+    if carry != 0: result = '1' + result
+
+    return result.zfill(max_len)
 
 
-print(arith_progression([5, 7, 9, 11]))
+print(add_binary_nums('11', '1'))
+print(add_binary_nums('10', '10'))
+print(add_binary_nums('111', '111'))
+print(add_binary_nums('1111111', '1'))
+
+"""
+31b. Write a Python program to add two binary numbers (into decimal).
+Input : ('11', '1')
+Output : 100
+"""
+
+
+def add_bin2(b1, b2):
+    return int(b1, 2) + int(b2, 2)
+
+
+print(add_bin2('11', '1'))
